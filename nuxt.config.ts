32a7ddc24737f83bfcd27d9ddb834ed5b8e4c170
AnTR <ttruong@adcleek.com>
2024-08-20 13:30:14 +0200

    feat(app): init slice machine and homepage slices

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,11 @@
+import { apiEndpoint, repositoryName } from "./slicemachine.config.json";
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: ["@nuxtjs/prismic"],
+
+  prismic: {
+    endpoint: apiEndpoint || repositoryName
+  }
 })
