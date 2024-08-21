9588300c11652cf953092ec790631b72fcce44ec
sdsmnc221 <antr.2201@gmail.com>
2024-08-21 03:36:03 +0200

    test(app): fetch data prsmic

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,19 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <main class="app">
+    <hero-banner-with-text
+      :slice="home?.data.slices[0]"
+    ></hero-banner-with-text>
+  </main>
 </template>
+
+<script setup lang="ts">
+import HeroBannerWithText from "../slices/HeroBannerWithText";
+
+const { client } = usePrismic();
+
+const { data: home } = await useAsyncData("home", () =>
+  client.getSingle("homepage")
+);
+
+console.log(home.value);
+</script>
--- slices/HeroBannerWithText/index.vue
+++ slices/HeroBannerWithText/index.vue
@@ -3,14 +3,16 @@ import { type Content } from "@prismicio/client";
 
 // The array passed to `getSliceComponentProps` is purely optional.
 // Consider it as a visual hint for you when templating your slice.
-defineProps(
+const props = defineProps(
   getSliceComponentProps<Content.HeroBannerWithTextSlice>([
     "slice",
     "index",
     "slices",
     "context",
-  ]),
+  ])
 );
+
+console.log(props.slice);
 </script>
 
 <template>
@@ -19,6 +21,6 @@ defineProps(
     :data-slice-variation="slice.variation"
   >
     Placeholder component for hero_banner_with_text (variation:
-    {{ slice.variation }}) Slices
+    {{ slice.variation }}) Slices toto
   </section>
 </template>
