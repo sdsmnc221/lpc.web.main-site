69e3c4828dfb041ebd79a6062f368f5e80262a6e
sdsmnc221 <antr.2201@gmail.com>
2025-01-01 08:31:19 +0100

    fix(app): adjust add links/avis

--- components/UiLinks/RightColumnLinks.vue
+++ components/UiLinks/RightColumnLinks.vue
@@ -18,7 +18,7 @@
           :key="link.linklabel"
           :href="
             link.linksrc.link_type !== 'Web'
-              ? `/${link.linksrc.uid}`
+              ? `/${link.linksrc.uid.toString().replace('-', '/')}`
               : link.linksrc.url
           "
           :target="link.linksrc.target ?? '_self'"
