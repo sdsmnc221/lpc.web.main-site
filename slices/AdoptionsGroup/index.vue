27029e76b40060ad67939055d00c5e6f4df78d41
sdsmnc221 <antr.2201@gmail.com>
2025-02-06 15:02:40 +0100

    fix(app): scrolltrigger cinfig

--- slices/AdoptionsGroup/index.vue
+++ slices/AdoptionsGroup/index.vue
@@ -436,13 +436,8 @@ const cleanupScrollTrigger = () => {
 };
 
 const configScrollTriggerSafari = () => {
-  if (isSafari()) {
-    ScrollTrigger.normalizeScroll({
-      allowNestedScroll: true,
-      lockAxis: true,
-      momentum: (self) => Math.min(3, self.velocityY / 10), // dynamically control the duration of the momentum when flick-scrolling
-      type: "touch,wheel,pointer", // now the page will be drag-scrollable on desktop because "pointer" is in the list
-    });
+  if (isIOS()) {
+    ScrollTrigger.normalizeScroll(true);
 
     ScrollTrigger.config({ ignoreMobileResize: true });
 
