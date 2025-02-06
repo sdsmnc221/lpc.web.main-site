b310c4d4d3166087083d0f70ef200f1192664318
sdsmnc221 <antr.2201@gmail.com>
2025-02-06 15:11:52 +0100

    test(app): up

--- slices/AdoptionsGroup/index.vue
+++ slices/AdoptionsGroup/index.vue
@@ -441,17 +441,17 @@ const configScrollTriggerSafari = () => {
 
     ScrollTrigger.config({ ignoreMobileResize: true });
 
-    // ScrollTrigger.observe({
-    //   target: section.value, // can be any element (selector text is fine)
-    //   type: "pointer,touch", // comma-delimited list of what to listen for ("wheel,touch,scroll,pointer")
-    //   // onUp: () => previous(),
-    //   // onDown: () => next(),
+    ScrollTrigger.observe({
+      target: section.value, // can be any element (selector text is fine)
+      type: "pointer,touch", // comma-delimited list of what to listen for ("wheel,touch,scroll,pointer")
+      // onUp: () => previous(),
+      // onDown: () => next(),
 
-    //   onDrag: (self) => {
-    //     console.warn(self.deltaX);
-    //     gsap.to(window, { scrollTo: { y: `+=${self.deltaX * 10}` } });
-    //   },
-    // });
+      onDrag: (self) => {
+        console.warn(self.deltaX);
+        gsap.to(window, { scrollTo: { y: `+=${self.deltaX * 10}` } });
+      },
+    });
   }
 };
 
