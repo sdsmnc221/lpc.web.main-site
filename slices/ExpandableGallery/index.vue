a50cbb80415e6b5aabf191212f3307901a6cb040
sdsmnc221 <antr.2201@gmail.com>
2025-02-15 10:29:01 +0100

    fix(app): transition flex expandebale gallery

--- slices/ExpandableGallery/index.vue
+++ slices/ExpandableGallery/index.vue
@@ -26,7 +26,7 @@
         <div
           v-for="(image, index) in chonk"
           :key="`expandable-gallery-${title}-${indexChonk}-${index}`"
-          class="relative flex h-full flex-1 cursor-pointer overflow-hidden aspect-square rounded-xl transition-all duration-500 ease-in-out hover:rounded-none md:hover:flex-[4] hover:flex-none hover:justify-center md:hover:h-full hover:h-auto hover:w-[100%]"
+          class="relative flex h-full flex-1 cursor-pointer overflow-hidden aspect-square rounded-xl transition-opacity transition-[flex] duration-500 ease-in-out hover:rounded-none md:hover:flex-[4] hover:flex-none hover:justify-center md:hover:h-full hover:h-auto hover:w-[100%]"
         >
           <img
             class="relative h-full w-full md:w-auto object-cover rounded-none pointer-events-none"
